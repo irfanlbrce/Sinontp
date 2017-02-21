ab066889e8ada344de6a0955b506e32f34407165
Mohammad Irfan <irfanlbrce@outlook.com>
2017-02-21 17:33:09 +0530

    Commiting the Changes

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,8 +6,11 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($log,$rootScope) {
 
+    $rootScope.$on('$stateChangeSuccess', function () {
+            document.body.scrollTop = document.documentElement.scrollTop = 0;
+        });
     $log.debug('runBlock end');
   }
 
--- src/app/news/updates/updates.controller.js
+++ src/app/news/updates/updates.controller.js
@@ -6,10 +6,26 @@
     .controller('UpdatesController', UpdatesController);
 
   /** @ngInject */
-  function UpdatesController() {
+  function UpdatesController($state) {
     var vm = this;
 
-    
+    vm.changeRoute = function(state){
+      if(state == 'SiNON wins'){
+        $state.go('news.sinon-wins-the-16th-annual-duke-startup-challenge');
+      }
+      else if(state == 'BRC'){
+        $state.go('news.brc-d2d-interview');
+      }
+       else if(state == 'SiNON Overcomes'){
+        $state.go('news.sinon-overcomes-barriers-to-win-grand-prize');
+      }
+       else if(state == 'Therapeutics'){
+        $state.go('news.therapeutics');
+      }
+      else{
+
+      }
+    }
     activate();
 
     function activate() {
