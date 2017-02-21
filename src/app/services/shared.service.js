0d335fb85e04c75579634a81f3baeee49f8a07e9
Surya Bhai <jsurya352@gmail.com>
2017-02-21 15:47:48 +0530

    worked on news pages and responsive ness

--- src/app/services/shared.service.js
+++ src/app/services/shared.service.js
@@ -13,8 +13,8 @@
         self.isCollapsedHorizontal = false;
         
         self.routes = [
-            { id: 1, state: 'news.sinon-wins-the-16th-annual-duke-startup-challenge', href: '#/news/sinon-wins-the-16th-annual-duke-startup-challenge', name: 'SiNON wins the 16th Annual Duke Startup Challenge – Entrepreneurship Week 2015', active: false, childContents: [], updatedDate:'2014-08-08T00:00:00+00:00',imgSrc:"" },
-            { id: 2, state: 'news.sinon-overcomes-barriers-to-win-grand-prize', href: '#/news/sinon-overcomes-barriers-to-win-grand-prize', name: 'SiNON Overcomes Barriers to Win Grand Prize', active: false, childContents: [], updatedDate:'2015-03-01T00:00:00+00:00',imgSrc:"" },
+            { id: 1, state: 'news.sinon-wins-the-16th-annual-duke-startup-challenge', href: '#/news/sinon-wins-the-16th-annual-duke-startup-challenge', name: 'SiNON wins the 16th Annual Duke Startup Challenge – Entrepreneurship Week 2015', active: false, childContents: [], updatedDate:'2014-08-08T00:00:00+00:00',imgSrc:"assets/images/Used/placeholder.png" },
+            { id: 2, state: 'news.sinon-overcomes-barriers-to-win-grand-prize', href: '#/news/sinon-overcomes-barriers-to-win-grand-prize', name: 'SiNON Overcomes Barriers to Win Grand Prize', active: false, childContents: [], updatedDate:'2015-03-01T00:00:00+00:00',imgSrc:"assets/images/Used/placeholder.png" },
             { id: 3, state: 'news.brc-d2d-interview', href: '#/news/brc-d2d-interview', name: 'BRC D2D Interview', active: false, childContents: [] , updatedDate:'2016-04-06T00:00:00+00:00',imgSrc:"assets/images/2016/04/brc-d2d-interview-160x160.jpg"},
             { id: 4, state: 'news.therapeutics', href: '#/news/therapeutics', name: 'Therapeutics', active: false, childContents: [], updatedDate:'2016-04-06T09:00:00+00:00',imgSrc:"assets/images/2016/04/therapeutics-160x160.jpg"},
             ];
